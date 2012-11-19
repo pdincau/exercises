217f602b051302214d1eb070edb74b606ca31d6b
Paolo <paolo.dincau@gmail.com>
2012-11-19 17:42:09 +0100

    add comments

--- bg_motion.py
+++ bg_motion.py
@@ -10,21 +10,27 @@ if __name__ == '__main__':
 		
 		cap = cv2.VideoCapture(video)
 		cv2.namedWindow("input video") 
-		
+
+		# Read first frame from video and set it as reference frame		
 		retval_bg, bg = cap.read()
+		# Convert to gray level
 		bg_gray = cv2.cvtColor(bg, cv.CV_BGR2GRAY)
 		
 		cv2.namedWindow("motion detected")
 
 		retval = True
 
+		# while we have frames in the video
 		while retval: 
 			retval, image = cap.read()
     			if retval:
+				# Convert to gray level the current frame
         			image_gray = cv2.cvtColor(image, cv.CV_BGR2GRAY)
 
+				# Compute difference with reference frame
 				diff = cv2.absdiff(bg_gray, image_gray)
 	
+				# Threshold difference
 				retval_diff, dst = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY);
 
 				cv2.imshow("input video", image)
